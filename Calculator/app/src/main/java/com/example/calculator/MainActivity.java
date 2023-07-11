b015f6832ba6641de1bc16de5f030f982b80da30
Gourav Kumar <91111185+gourav2982@users.noreply.github.com>
2023-07-11 19:19:41 +0530

    done with ui

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.edittext);
+        editText.setShowSoftInputOnFocus(false);
     }
 }
