871bf23fff9a8c97fade93f7e933efb03905c0d9
Gourav Kumar <91111185+gourav2982@users.noreply.github.com>
2023-07-20 17:19:24 +0530

    done with all the ui

---
+++ Quizz_App/app/src/main/java/com/example/quiz_app/model/Score_screen.java
@@ -0,0 +1,16 @@
+package com.example.quiz_app.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quiz_app.R;
+
+public class Score_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_screen);
+    }
+}
